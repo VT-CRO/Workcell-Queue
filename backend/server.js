7b494c858a9c1e9472ace3c4fa57a36ad5f79cbf
rbgary0 <reagangary14@gmail.com>
2025-01-26 13:21:55 -0500

    updated

--- backend/server.js
+++ backend/server.js
@@ -34,7 +34,7 @@ const OUTPUT_ORCA_PRINTER_DIR = path.join(__dirname, 'outputs');
 const PRINTER_HOST = `${process.env.FRONTEND_URL}/api`;
 const DEFAULT_FILAMENT = "Generic PLA template @Voron v2 300mm3 0.4 nozzle"
 const DEFAULT_PROCESS = "0.20 Standard"
-const VERSION = "1.0.3"
+const VERSION = "1.0.4"
 
 // Path
 const queueFilePath = path.join(__dirname, 'printQueue.json');
@@ -167,7 +167,7 @@ const verifyGuildMembership = async (req, res, next) => {
       if (!isMember) {
 
         // delete user in firestore --> still needs to be tested
-        const docRef = db.doc("users", req.session.user.id);
+        const docRef = db.collection("users").doc(req.session.user.id);
     
         deleteDoc(docRef)
           .catch((error) => {
