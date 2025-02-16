b52fb19808b86e9e18a1ea0baba28097e8bf0e19
rbgary0 <reagangary14@gmail.com>
2025-02-16 15:40:54 -0500

    Added limited Admin Functionality

---
+++ frontend/src/pages/Admin.jsx
@@ -0,0 +1,54 @@
+import { useState, useEffect } from 'react';
+import UserCard from '../components/UserCard.jsx';
+
+
+const backendUrl = import.meta.env.VITE_BACKEND_URL;
+
+const Admin = () => {
+  const [Admin, setAdmin] = useState(null);
+  const [Users, setUsers] = useState([]);
+  const [Error, setError] = useState(null);
+
+  useEffect(() => {
+    // Fetch user data
+    fetch(`${backendUrl}/users`, { credentials: 'include' })
+      .then((res) => {
+        if (res.status === 401) {
+          setError('Unauthorized access. Please log in.');
+          return null;
+        }
+        if (!res.ok) {
+          throw new Error('Failed to fetch user data.');
+        }
+        return res.json();
+      })
+      .then((data) => {
+        if (data.isAdmin == false) {
+          setAdmin(null);
+        }
+        else {
+          setAdmin(data);
+        }
+        setError(null); // Clear any existing error
+      })
+      .catch((err) => {
+        console.error(err);
+        setError('An error occurred while fetching user data.');
+      });
+  }, []);
+
+
+  return (
+    <>
+      {Admin ? <> {Admin.map((Admin) => (
+        <div key={Admin.id}>
+        <UserCard user={Admin} />
+        </div>
+      ))} </> 
+      : 
+      <>You must be an admin to access this page</>}
+    </>
+  );
+};
+
+export default Admin;
